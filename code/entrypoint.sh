1ab242bf5955dae069c9baef2f8738a60fc51c66
kenlight-bu <kenlight@bu.edu>
2024-09-15 14:12:47 -0400

    I had to use a tool to substitue heroku's environment variables.  Let's see if this works

---
+++ code/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Substitute environment variables into the Nginx configuration
+envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Start Nginx
+nginx -g 'daemon off;'
