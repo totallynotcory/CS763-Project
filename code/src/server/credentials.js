55614a4325dc52b0d62939fd291a300463a5fbd4
Chris Ceravolo <137582506+ccerav-bu@users.noreply.github.com>
2024-09-17 17:58:35 -0400

    Update credentials.js

--- code/src/server/credentials.js
+++ code/src/server/credentials.js
@@ -1,4 +1,4 @@
 //need to store this securely, perhaps as environment variable
 module.exports = {
-    MONGO_URI: "mongodb+srv://ccerav:Team3Secret@cluster0.tluzy.mongodb.net/"
+    MONGO_URI: "mongodb+srv://team3:Team3Secret@cluster0.tluzy.mongodb.net/"
 }
