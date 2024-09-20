ff3a479ad9d4878e27fedd0e3c7918ea0175a16f
Amanda Yee <amandakyee@gmail.com>
2024-09-20 18:04:21 -0400

    add in goal schema and dummy data

--- code/server/initdb.js
+++ code/server/initdb.js
@@ -3,48 +3,60 @@
 const db = require("./db.js")
 
 const User = db.getModel().userModel
+const Goal = db.getModel().goalModel
 
 async function init() {
-
     try {
-
-        // //Option to delete existing data for full reset
+        // Option to delete existing data for full reset
         // await User.deleteMany({})
-        
+        // await Goal.deleteMany({})
+
+        let goal1 = new Goal({
+            goalId: "G10001",
+            type: "sleep",
+            targetValue: 8,
+            unit: "hours",
+            createdAt: "2024-09-10",
+            progress: [{date: new Date("2024-09-11"), value: 7.5},
+                        {date: new Date("2024-09-12"), value: 8}]
+        })
+
         let user1 = new User({
             userId: "U10001",
             email: "test@gmail.com",
-            password_hashed: "pa55w0rd",
+            passwordHashed: "pa55w0rd",
             name: "John Smith",
             createdAt: "2024-09-01",
-            goals: []
+            goals: [goal1]
         })
+
         let user2 = new User({
             userId: "U10002",
             email: "hello@bu.edu",
-            password_hashed: "b0st0nuniversity",
+            passwordHashed: "b0st0nuniversity",
             name: "Jane Doe",
             // createdAt: "2024-09-01", // testing the default param in schema works
             goals: []
         })
+
+        
     
         await Promise.all([
             user1.save(),
-            user2.save()
+            user2.save(),
+            goal1.save()
         ])
     
         let users = await User.find({})
-    
         console.log(users)
 
+        let goals = await Goal.find({})
+        console.log(goals)
+
     } catch(error) {
-
         console.log(error)
-
     } finally {
-
         db.closeConnection()
-
     }
 }
 
