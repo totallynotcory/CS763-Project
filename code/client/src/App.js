ff0b60e9ceb28abdf16b669574e6620bb480e55c
abbiekuma <abbieluo.yu@gmail.com>
2024-09-22 22:04:54 -0400

    add dailydata page and use mui

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Home from './components/Home.js'
-import CreateUser from './components/CreateUser.js'
-import ViewUsers from './components/ViewUsers.js'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home.js";
+import CreateUser from "./components/CreateUser.js";
+import ViewUsers from "./components/ViewUsers.js";
+import DailyData from "./components/DailyData.js";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import "@fontsource/mulish";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Mulish', sans-serif", // 全局设置 Mulish 字体
+  },
+});
 
 function App() {
-  
-
   return (
-    <Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
         <nav>
           <ul>
             <li>
@@ -20,16 +29,21 @@ function App() {
             <li>
               <Link to="/view-users">View Users</Link>
             </li>
+            <li>
+              <Link to="/enter-daily-data">Daily Data</Link>
+            </li>
           </ul>
         </nav>
-        
+
         <Routes>
           <Route path="/" element={Home()} />
           <Route path="/create-user" element={CreateUser()} />
           <Route path="/view-users" element={ViewUsers()} />
+          <Route path="/enter-daily-data" element={DailyData()} />
         </Routes>
-    </Router>
-  )
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
