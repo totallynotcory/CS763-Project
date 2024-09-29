0e2facac894c9c38e1c71d075fd026a4fb3cf70c
Amanda Yee <amandakyee@gmail.com>
2024-09-29 15:24:38 -0400

    add test that has valid inputs

--- code/client/src/__tests__/validateRegistrationForm.test.js
+++ code/client/src/__tests__/validateRegistrationForm.test.js
@@ -1,4 +1,4 @@
-import { validateRegistrationForm } from '../utils/validateRegistrationForm'; // Update with your file path
+import { validateRegistrationForm } from '../utils/validateRegistrationForm'; 
 
 test('empty input form fields', () => {
     const formData = { name: '', email: 'test@example.com', passwordHashed: 'password123' };
@@ -22,4 +22,10 @@ test('email missing first part of domain', () => {
     const formData = { name: 'Test', email: 'test@.com', passwordHashed: 'password123' };
     const result = validateRegistrationForm(formData);
     expect(result).toBe(false);
+});
+
+test('valid input form fields', () => {
+    const formData = { name: 'Valid Test', email: 'test@gmail.com', passwordHashed: 'password123' };
+    const result = validateRegistrationForm(formData);
+    expect(result).toBe(true);
 });
--- code/client/src/utils/validateRegistrationForm.js
+++ code/client/src/utils/validateRegistrationForm.js
@@ -1,19 +1,22 @@
 export function validateRegistrationForm(formData) {
     const { name, email, passwordHashed } = formData;
 
-    // Checking input data in each field
+    // Checking there is input data in each field
     if (!name || !email || !passwordHashed) {
       console.log('Form field(s) missing')  
       return false;
     }
-  
-    // Email format validation
+
+    // Email format validation:
+    // Checking email starts with 1+ alphanumeric characters, "." or "-" followed by an "@"
+    // followed by 1+ alphanumeric characters or hyphens (domain) followed by a "."
+    // ending with 2 to 4 characters (top-level domain)
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/;
     if (!emailRegex.test(email)) {
       console.log('Invalid email format')
       return false;
     }
-  
+
     // If all validations pass
     return true;
-  }
+}
