26149720676e06a03de8c91e6651b0338b954395
kenlight-bu <kenlight@bu.edu>
2024-09-29 18:57:59 -0400

    and another try to get environment variables working on the client side

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log("API Base URL:", process.env.REACT_APP_BASE_URL);
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development environment
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development
 });
 
 export default apiClient;
