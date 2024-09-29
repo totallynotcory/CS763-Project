542db3562c853a2328618b42d0e3540ccca59595
kenlight-bu <kenlight@bu.edu>
2024-09-29 17:03:41 -0400

    Had to install dotenv on the client side to reference Envinronment variables

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+const dotenv = require('dotenv');
 
 const apiClient = axios.create({
   baseURL: process.env.BASE_URL || 'http://localhost:5000', // Base URL for API requests, update with environemnt variable for deployment
