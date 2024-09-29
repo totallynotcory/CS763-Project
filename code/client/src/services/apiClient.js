9a689ce7430bfaf67d60b6501072614118a16020
kenlight-bu <kenlight@bu.edu>
2024-09-29 18:26:38 -0400

    Making a small change to force ci/cd in GitHub

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development environment
 });
 
 export default apiClient;
