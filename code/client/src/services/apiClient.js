c06658fb01167f50f1bb1a961c2b0c01a364c3e0
kenlight-bu <kenlight@bu.edu>
2024-09-28 21:53:36 -0400

    and another try

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+// const apiClient = axios.create({
+//   baseURL: process.env.BASE_URL || 'http://localhost:5000', // Base URL for API requests, update with environemnt variable for deployment
+// })
+
 const apiClient = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:5000', // Base URL for API requests, update with environemnt variable for deployment
+  baseURL: 'https://peak-performance-3c40e2c0ebea.herokuapp.com/', // Base URL for API requests, update with environemnt variable for deployment
 })
 
 export default apiClient
