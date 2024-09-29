1872da9209df1583e22c7877c42019007527147b
kenlight-bu <kenlight@bu.edu>
2024-09-29 17:10:57 -0400

    Hopefully fixed the environment file situation

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 const dotenv = require('dotenv');
 
+// Determine the correct .env file based on the environment
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+
+// Load the appropriate .env file
+// dotenv.config({ path: `../${envFile}` }); 
+dotenv.config({ path: path.resolve(__dirname, `../../../${envFile}`) })
+
 const apiClient = axios.create({
   baseURL: process.env.BASE_URL || 'http://localhost:5000', // Base URL for API requests, update with environemnt variable for deployment
 })
