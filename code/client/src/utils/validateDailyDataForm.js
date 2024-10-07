dadb474ec19b4c5d9c91dc6e22800226f5f6223e
Amanda Yee <amandakyee@gmail.com>
2024-10-06 22:35:09 -0400

    form validation for daily data page

---
+++ code/client/src/utils/validateDailyDataForm.js
@@ -0,0 +1,16 @@
+export function validateDailyDataForm(formData) {
+    const { weight, steps, sleep, water, exercise, entryDate } = formData;
+
+    // Checking there is input data in each field
+    if (!weight || !steps || !sleep || !water || !exercise || !entryDate) {
+        return { isValid: false, message: 'Form field(s) missing' };
+    }
+
+    // Checking validity of integer inputs
+    if (weight < 0 || steps < 0 || sleep < 0 || water < 0 || exercise < 0) {
+        return { isValid: false, message: 'You cannot enter negative values for health metrics.' };
+    }
+
+    // If all validations pass
+    return { isValid: true, message: 'Form is valid' };
+}
