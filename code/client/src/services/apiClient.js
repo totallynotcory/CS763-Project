0c9b098852f861497d44197624df958e6e449914
eddie27lee <33395221+eddie27lee@users.noreply.github.com>
2024-10-08 23:32:25 -0400

    Update apiClient.js

--- code/client/src/services/apiClient.js
+++ code/client/src/services/apiClient.js
@@ -1,19 +1,5 @@
 import axios from 'axios';
 
-// console.log("API Base URL:", process.env.REACT_APP_BASE_URL);
-
-// const apiClient = axios.create({
-//   baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5000', // Default to localhost for development
-// });
-
-// const apiClient = axios.create({
-//   baseURL: 'https://peak-performance-backend-394f316db343.herokuapp.com/', // Default to localhost for development
-// });
-
-// const apiClient = axios.create({
-//     baseURL: 'http://localhost:5000', // Default to localhost for development
-//   });
-
 var serverURL = null
 
 switch (process.env.NODE_ENV) {
