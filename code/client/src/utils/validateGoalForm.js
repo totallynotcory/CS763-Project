6c78e26d51896f8857948d33f663fb170f516ed2
Amanda Yee <amandakyee@gmail.com>
2024-10-12 13:43:37 -0400

    add additional form validations

--- code/client/src/utils/validateGoalForm.js
+++ code/client/src/utils/validateGoalForm.js
@@ -2,8 +2,8 @@ export function validateGoalForm(formData) {
     const { type, targetValue } = formData;
 
     if (typeof type !== 'string' || type.trim() === '') {
-        console.log('Type is required and must be a non-empty string');
-        return { isValid: false, message: 'Type is required and must be a non-empty string' };
+        console.log('Goal type is required and must be a non-empty string');
+        return { isValid: false, message: 'Goal type is required and must be a non-empty string' };
     }
 
     // Convert targetValue to a number
@@ -12,6 +12,17 @@ export function validateGoalForm(formData) {
     if (typeof numericValue !== 'number' || isNaN(numericValue) || numericValue < 0) {
         console.log('Target value must be a valid number greater than or equal to zero');
         return { isValid: false, message: 'Target value must be a valid number greater than or equal to zero' };
+    }
+
+    // Limit goals to not exceed the amount of time in a day
+    if (type == 'sleep' & numericValue > 24) {
+        console.log('Sleep goal cannot exceed 24 hours');
+        return { isValid: false, message: 'Sleep goal cannot exceed 24 hours' };
+    }
+
+    if (type == 'exercise' & numericValue > 1440) {
+        console.log('Exercise goal cannot exceed 1440 minutes');
+        return { isValid: false, message: 'Exercise goal cannot exceed 1440 minutes (number of minutes in a day)' }; 
     }
 
     // If all validations pass
